dcb11ddc8e13626bf975ceb1fd0f59365d5cded6
isabelrem <irt99@protonmail.com>
2026-03-04 11:50:31 +0000

    step_1_and_2 v1.5 with separated bwa mem and picard sort sam - attempting to fix silent OOM errors

--- step_1_and_2/src/code.sh
+++ step_1_and_2/src/code.sh
@@ -1,49 +1,47 @@
 #!/bin/bash
-
-# The following line causes bash to exit at any point if there is any error
-# and to output each line as it is executed -- useful for debugging
 set -e -x -o pipefail
 
-#Grab inputs
+# Grab inputs
 dx-download-all-inputs --except ref_genome --parallel
 
-# make output folders
+# Make output folders
 mkdir -p ~/out ./genome ~/out/bam_file ~/out/bai_file
 
-# make directory for reference genome and unpackage the reference genome
+# Reference genome
 dx cat "$ref_genome" | tar -xzf - -C genome  
-	
-echo ${umi_sequence_prefix} # also works - umi filename
-describer=$(echo ${umi_sequence_prefix} | sed -e 's/_R2_001//') #${describer}
+
+echo ${umi_sequence_prefix}
+describer=$(echo ${umi_sequence_prefix} | sed -e 's/_R2_001//')
 echo ${describer}	
 
-sample=$(echo ${describer}| grep -o 'SCIP[0-9]*' | tail -n 1)
+sample=$(echo ${describer} | grep -o 'SCIP[0-9]*' | tail -n 1)
 echo ${sample}
 
-# Calculate 60% of available RAM for Java heap (safe buffer)
+# Calculate 60% of available RAM for Java heap
 AVAILABLE_RAM_GB=$(free -g | awk '/^Mem:/{print int($2 * 0.6)}')
 JAVA_THREADS=$(( $(nproc) / 4 ))
 [ ${JAVA_THREADS} -lt 2 ] && JAVA_THREADS=2
+JAVA_OPTS="-Xmx${AVAILABLE_RAM_GB}g -XX:+UseParallelGC -XX:ParallelGCThreads=${JAVA_THREADS} -XX:+UseAdaptiveSizePolicy -Djava.io.tmpdir=/home/dnanexus"
 
-# this was used attempt 1
-# Set JVM options variably dependent on instance type and resource available
-JAVA_OPTS="-Xmx${AVAILABLE_RAM_GB}g -XX:+UseParallelGC -XX:ParallelGCThreads=${JAVA_THREADS} -XX:+UseAdaptiveSizePolicy -Djava.io.tmpdir=/home/dnanexus "
-
-# step 1: align, step 2: compress and sort
+# Step 1: BWA align to unsorted BAM
 bwa mem -M -t $(nproc) genome/genome.fa \
   ${fastq_forward_reads_r1_path} \
-  ${fastq_reverse_reads_r3_path} | \
+  ${fastq_reverse_reads_r3_path} \
+  > /home/dnanexus/${describer}.unsorted.bam
+
+# Step 2: Picard sort (now has full RAM to itself)
 java ${JAVA_OPTS} -Djava.awt.headless=true -jar ${picard_jar_path} SortSam \
-  I=/dev/stdin \
+  I=/home/dnanexus/${describer}.unsorted.bam \
   O=~/out/bam_file/${describer}.bam \
   SORT_ORDER=coordinate \
-  MAX_RECORDS_IN_RAM=200000 \
-  COMPRESSION_LEVEL=5 
+  COMPRESSION_LEVEL=5 \
+  TMP_DIR=/home/dnanexus
 
-# index bam
-samtools index ~/out/bam_file/${describer}.bam ~/out/bai_file/${describer}.bam.bai 
+# Clean up intermediate file to free disk space
+rm /home/dnanexus/${describer}.unsorted.bam
 
-# upload outputs
+# Index BAM
+samtools index ~/out/bam_file/${describer}.bam ~/out/bai_file/${describer}.bam.bai
+
+# Upload outputs
 dx-upload-all-outputs --parallel
-
-
